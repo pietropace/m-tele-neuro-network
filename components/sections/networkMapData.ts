9d7a5c07599aa930a3162441486abd4ee4d260f3
Nome Cognome Secondo Account <33632373+pietropace@users.noreply.github.com>
2026-05-11 14:48:46 +0200

    Move Sciacca marker southeast

--- components/sections/networkMapData.ts
+++ components/sections/networkMapData.ts
@@ -14,7 +14,7 @@ export const sharedMapPoints = {
   bari: { x: 746, y: 522 },
   torino: { x: 212, y: 218 },
   nervi: { x: 244, y: 298 },
-  sciacca: { x: 455, y: 738 },
+  sciacca: { x: 505, y: 758 },
   tradate: { x: 289, y: 185 },
 } satisfies Record<string, SharedMapPoint>;
 
